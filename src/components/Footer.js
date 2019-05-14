dd4f63b7854ff7f29ebf214403d4998708250209
alexanderjacks <alexanderthejacks@gmail.com>
2019-05-14 14:40:35 -0700

    :wrench: actually xmit changes from last commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -68,14 +68,14 @@ const Footer = class extends React.Component {
                 </section>
               </div>
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://www.facebook.com/minorityvets/">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com/minorityvets">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -83,7 +83,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://www.instagram.com/minorityvets/">
                   <img
                     src={instagram}
                     alt="Instagram"
